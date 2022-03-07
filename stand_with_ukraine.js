e7a0cfa2e012d1a071f52494c11c90cf371f6302
Frank Bültge <frank@bueltge.de>
2022-03-07 12:05:21 +0100

    Use vanilla javascript

--- stand_with_ukraine.js
+++ stand_with_ukraine.js
@@ -1,4 +1,7 @@
-jQuery(document).ready(function ($) {
-	let body = $('body');
-	body.prepend('<div id="stand_with_ukraine_overlay"><a href="https://war.ukraine.ua/">#StandWithUkraine</a></div>');
+document.addEventListener('DOMContentLoaded', function() {
+	let body = document.getElementsByTagName('body')[0];
+	let div  = document.createElement('div');
+	div.id = 'stand_with_ukraine_overlay';
+	div.innerHTML = '<a href="https://war.ukraine.ua/">#StandWithUkraine</a>';
+	document.body.insertBefore(div, body.firstChild);
 });
