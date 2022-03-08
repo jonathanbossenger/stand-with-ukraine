1d432d33378de5f738b0652e64eb188bbdb2eda6
Erik Golinelli <erik@codekraft.it>
2022-03-08 22:22:20 +0100

    js: fixes document.getElementsByTagName('body')[0].firstchild - Argument type ChildNode is not assignable to parameter type Node

--- stand_with_ukraine.js
+++ stand_with_ukraine.js
@@ -1,7 +1,7 @@
+/* global swu_options */
 document.addEventListener('DOMContentLoaded', function() {
-	let body = document.getElementsByTagName('body')[0];
-	let div  = document.createElement('div');
+	var div  = document.createElement('div');
 	div.id = 'stand_with_ukraine_overlay';
 	div.innerHTML = '<a title="' + swu_options.text + '" href="' + swu_options.url + '">' + swu_options.hashtag + '</a>';
-	document.body.insertBefore(div, body.firstChild);
+	document.body.insertAdjacentElement('beforebegin', div);
 });
