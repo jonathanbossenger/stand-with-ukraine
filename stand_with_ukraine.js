99ea3d3fe037f7fa81df570806b411a3c1e4a764
Erik Golinelli <erik@codekraft.it>
2022-03-10 13:25:23 +0100

    puts the banner in exactly the same place as before

--- stand_with_ukraine.js
+++ stand_with_ukraine.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
 	var div  = document.createElement('div');
 	div.id = 'stand_with_ukraine_overlay';
 	div.innerHTML = '<a title="' + swu_options.text + '" href="' + swu_options.url + '">' + swu_options.hashtag + '</a>';
-	document.body.insertAdjacentElement('beforebegin', div);
+	document.body.insertAdjacentElement('afterbegin', div);
 });
